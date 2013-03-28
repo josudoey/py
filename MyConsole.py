6ceec571b9f4593774af641568e0e87d53b55d39
Chih-Yi <cyfang@cs.nctu.edu.tw>
2013-03-28 15:57:38 +0800

    update

--- MyConsole.py
+++ MyConsole.py
@@ -1,65 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
+# ref http://zetcode.com/lang/python/functions/
+def get_func(obj):
+    from inspect import isfunction
+    funcs=list()
+    for fn in dir(obj):
+        func=getattr(obj,fn)
+        if isfunction(func):
+            funcs.append((fn,func))
+    return  dict(funcs)
+
 import sys
 class MyConsole:
     """aaaa"""
     def    __init__(self):
-        self.mode="main"
-        self.funcs=self.get_func("do_"+self.mode)
-        self.show_func=getattr(self,"show_"+self.mode)
-    def get_func(self,prefix):
-        funcs=list()
-        for fn in dir(self):
-             if fn.startswith(prefix):
-                func=getattr(self,fn)
-                docs=func.__doc__.split("\n",2)
-                funcs.append((docs[0],func))
-        return  dict(funcs)
-
-    def show_opts(self):
-        for key in self.funcs:
-                f=self.funcs[key]
-                docs=f.__doc__.split("\n",2)
-                cmd=docs[0]
-                text=docs[1]
-                print "[%s]%s"%(str(key),text)
-
-    def show_main(self):
-        print "main function"
-        self.show_opts()
-
-    def input_func(self):
-        self.show_func()
-        sys.stdout.write("input:")
-        line=sys.stdin.readline()[:-1]
-        if len(line)==0:
-                return
-        opts=line.split(" ",2)[0]
-        funcs=self.funcs
-        if funcs.has_key(opts):
-                funcs[opts](line)
-        else:
-                print "[%s] no option"%(opts)
-
-
-    def main(self):
         while 1:
-          self.input_func()
+            self.run()
 
-
-    def do_main_test(self,line):
-        """a
-        test: testaaa"""
-        print "test"
-    def do_main_aexit(slef,line):
-       """b
-(q)    exit"""
-       sys,exit(0)
-
+    def run(self):
+        print sys.stdin.read(1)
 
 def main():
         c=MyConsole()
         c.main()
 
 if __name__ == "__main__":
-    main()
-
+   main()
+   #print __name__
+   # print locals()
+   # print ch.get_func(locals()["__main__"]).keys()
+   # print ch.get_func(ch).keys()
+    #main()
