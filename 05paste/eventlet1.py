b8e8fa081a757e0f0eeb10433949cbe0a3ff7460
fajoy <wuminfajoy@gmail.com>
2013-12-30 12:03:36 +0800

    update eventlet example

--- 05paste/eventlet1.py
+++ 05paste/eventlet1.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 import os
-from eventlet import wsgi
 import eventlet 
 from paste.deploy import loadapp  
 from paste.deploy import loadserver  
 import eventlet
 import time 
 import eventlet 
+from eventlet import wsgi
 from eventlet import greenthread
+from eventlet import event
+eventlet.monkey_patch() 
 
 import logging
 import os
 log = logging.getLogger(__name__)
 
-
-from eventlet import event
 
 
 def background(_pool,*args,**kwargs): 
@@ -25,7 +25,6 @@ def background(_pool,*args,**kwargs):
 
 
 def server_start(_scok,_app_pool,*args,**kwargs):
-    eventlet.monkey_patch() 
     wsgi.server( _sock
                 ,_app
                 ,custom_pool=_pool
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     _app = loadapp("config:%s" % os.path.abspath(configfile), appname)  
     _sock=eventlet.listen(('0.0.0.0', 8090))
 
-    _pool = eventlet.GreenPool(1000)  
+    _pool = eventlet.GreenPool(100)  
     _pool.spawn_n(background,_pool) 
     _pool.spawn(server_start,_sock,_app,_pool) 
     ent = event.Event()
