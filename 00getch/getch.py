5c217ce2a52127892d6f5bc346a7bf08e00162a1
fajoy <wuminfajoy@gmail.com>
2014-04-25 18:51:33 +0800

    add get char

---
+++ 00getch/getch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import sys    
+import termios
+import fcntl
+
+def getch():
+  fd = sys.stdin.fileno()
+
+  oldterm = termios.tcgetattr(fd)
+  newattr = termios.tcgetattr(fd)
+  newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+  termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+  oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+  fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+  try:        
+    while 1:            
+      try:
+        c = sys.stdin.read(1)
+        break
+      except IOError: pass
+  finally:
+    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+  return c
+
+def main():
+    while True:
+         print getch().encode("hex")
+
+if __name__=="__main__":
+    main()
