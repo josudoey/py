a9416f77b10402ef4ef5d887437216bc42aae918
joey fang <joey@vm5.com>
2014-06-05 11:59:20 +0800

    modify hello.

--- 00hello/hello.py
+++ 00hello/hello.py
@@ -5,29 +5,18 @@ import logging
 from collections import OrderedDict
 from fajoy import config
 log = logging.getLogger()
+log.setLevel(logging.WARNING)
 
-def enable_log(path):
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def enable_log(path,logger=log,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
     fn=os.path.normpath(os.path.join(os.path.dirname(__file__),path))
     if os.path.isabs(path):
         fn=path
     hdlr = logging.FileHandler(fn)
-    #hdlr.setLevel(logging.DEBUG)
-    hdlr.setFormatter(formatter)
+    hdlr.setFormatter(format)
     log.addHandler(hdlr)
-    log.warning("enable log in %s "%fn)
-
-def enable_verbose():
-    log.setLevel(logging.INFO)
-    log.info("enable verbose.")
-
-def enable_debug():
-    log.setLevel(logging.DEBUG)
-    log.debug("enable debug.")
+    log.info("enable log in %s "%fn)
 
 def main():
-    logging.basicConfig( stream=sys.stderr , level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
     parser = argparse.ArgumentParser(description='this is hello example.')
 
     parser.add_argument('name', metavar='NAME',type=str, nargs='?',default=None,
@@ -44,18 +33,24 @@ def main():
 
     args = parser.parse_args()
 
-    if args.verbose:
-        enable_verbose()
-
-    if args.debug:
-        enable_debug()
-
     conf=config.defaults()
-    if log.isEnabledFor(logging.DEBUG):
-        log.debug(conf)
 
     if config.has_section("fajoy"):
         conf=OrderedDict(config.items("fajoy"))
+
+
+    log_fmt="%(asctime)s - [%(threadName)s:%(name)s.%(funcName)s((%(lineno)d))] - %(levelname)s - %(message)s"
+
+    log_fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    if conf.has_key("log_format"):
+        log_fmt=conf["log_format"]
+
+    if args.debug:
+        logging.basicConfig(stream=sys.stderr , level=logging.DEBUG, format=log_fmt)
+
+    if args.verbose:
+        logging.basicConfig(stream=sys.stderr , level=logging.INFO, format=log_fmt)
+    logging.basicConfig(stream=sys.stderr , level=logging.WARNING, format=log_fmt)
 
     if conf.has_key("log"):
         enable_log(conf["log"])
@@ -63,8 +58,14 @@ def main():
     if log.isEnabledFor(logging.INFO):
         log.info(conf)
 
+    if log.isEnabledFor(logging.DEBUG):
+        log.debug(conf)
+
     name = args.name or "%s%s" % (conf.get("name"),"(%s)" % conf.get("mail") if conf.has_key("mail") else "")
     msg = " ".join(args.msg) or "hello"
+
+    e = Exception("e")
+    log.exception(e)
     print "%s : %s" %(name,msg)
 
 if __name__=="__main__":
